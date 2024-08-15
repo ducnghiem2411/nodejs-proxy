a0e2f875b8356db9eb118fb0003b160735c58e9e
nghiemld <nghiemld@theparallel.io>
2024-08-15 15:58:19 +0700

    add proxy port

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ const express = require('express')
 import { createProxyMiddleware } from 'http-proxy-middleware'
 require('dotenv').config()
 
-const TARGET = process.env.TARGET
-const PORT = process.env.PORT
+const TARGET_1 = process.env.TARGET_1
+const PORT_1 = process.env.PORT_1
 
-if (!TARGET) {
-    throw new Error('TARGET is not defined')
+if (!TARGET_1) {
+    throw new Error('TARGET_1 is not defined')
 }
-if (!PORT) {
-    throw new Error('PORT is not defined')
+if (!PORT_1) {
+    throw new Error('PORT_1 is not defined')
 }
 
 const app = express()
@@ -17,7 +17,7 @@ const app = express()
 app.use(
     "/",
     createProxyMiddleware({
-        target: TARGET,
+        target: TARGET_1,
         changeOrigin: true,
         on: {
             proxyReq: (proxyReq) => {
@@ -41,6 +41,50 @@ app.use(
 )
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`)
+app.listen(PORT_1, () => {
+    console.log(`App_1 running on PORT ${PORT_1}`)
 })
+
+const TARGET_2 = process.env.TARGET_2
+const PORT_2 = process.env.PORT_2
+
+if (!TARGET_2) {
+    throw new Error('TARGET_2 is not defined')
+}
+if (!PORT_2) {
+    throw new Error('PORT_2 is not defined')
+}
+
+const app2 = express()
+
+app2.use(
+    "/",
+    createProxyMiddleware({
+        target: TARGET_2,
+        changeOrigin: true,
+        on: {
+            proxyReq: (proxyReq) => {
+                // Enable CORS
+                proxyReq.setHeader('Access-Control-Allow-Origin', '*')
+                console.log(`Forwarded ${proxyReq.method} ${proxyReq.protocol}//${proxyReq.host}${proxyReq.path}`)
+
+                // Remove header
+                proxyReq.removeHeader('User-Agent')
+                proxyReq.removeHeader('Origin')
+                proxyReq.removeHeader('Referer')
+            },
+
+            proxyRes: (proxyRes, req, res) => {
+                res.setHeader('Access-Control-Allow-Origin', '*')
+                res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            }
+        },
+    }),
+)
+
+
+app2.listen(PORT_2, () => {
+    console.log(`App_2 running on PORT ${PORT_2}`)
+})
+
